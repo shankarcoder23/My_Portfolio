7a2966bbdeb64344bc67db40faedf7eaf97eb291
shankarcoder23 <shankar.coder23@gmail.com>
2025-12-24 13:31:09 +0530

    updated

--- Assets/JS/portfolio.js
+++ Assets/JS/portfolio.js
@@ -1,15 +1,16 @@
 
-  const typingElement = document.querySelector(".typing");
-  const text = typingElement.getAttribute("data-text");
-  let index = 0;
+const text = "Shankar Das";
+let index = 0;
+const speed = 120;
+const typingElement = document.querySelector(".typing");
 
-  function typeEffect() {
-    if (index < text.length) {
-      typingElement.textContent += text.charAt(index);
-      index++;
-      setTimeout(typeEffect, 120);
-    }
+function typeEffect() {
+  if (index < text.length) {
+    typingElement.textContent += text.charAt(index);
+    index++;
+    setTimeout(typeEffect, speed);
   }
+}
 
-  typeEffect();
+typeEffect();
 
